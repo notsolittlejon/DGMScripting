47ec69d3f694535e4176a175de34c5d7a6efe8ca
Jonathan Hardy <thembwun@gmail.com>
2016-05-23 11:08:39 -0600

    Created Syntax script

---
+++ SummerProject/Assets/Syntax.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Syntax : MonoBehaviour{
+
+	int myInt;
+
+	int MyInt(int _i, int _j){
+		return _i+_j;
+	}
+
+	void Start (){
+		myInt = MyInt (10,4);
+		print (myInt);
+	}
+
+
+}
