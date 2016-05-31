f2f48ad9b028928f69fbcbf5496985c19e87c0ad
Jonathan Hardy <thembwun@gmail.com>
2016-05-31 11:35:16 -0600

    Created Branch1 script

---
+++ SummerProject/Assets/Branch1.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Branch1 : MonoBehaviour {
+	int branch1 = 3;
+	int branch2 = 3;
+	//Simulates merging two branches. How creative. 
+	int Merge (int _alpha, int _bravo){
+		return _alpha + _bravo;
+	}
+	//-------------------------------------
+	void Start () {
+		print(Merge (branch1, branch2));
+	}
+	
+	// Update is called once per frame
+	//void Update () {
+	
+	//}
+}
