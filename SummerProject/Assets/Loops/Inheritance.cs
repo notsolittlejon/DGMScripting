bda56ed858276d449b669e69d3185de880866e14
Jonathan Hardy <thembwun@gmail.com>
2016-06-13 11:32:45 -0600

    Loops, Inheritance, Inheritance 2 scripts created
    Also organized assets into folders a bit better

---
+++ SummerProject/Assets/Loops/Inheritance.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class BaseAmmo {
+	//This controls the amount of ammo and power level of ammo.
+	public int ammoCount;
+	public int ammoPower;
+
+	public void Fire(){
+		if (ammoCount > 0) {
+			ammoCount--;
+			Debug.Log (ammoCount);
+		} else {
+			Debug.Log ("Out of Ammo");
+		}
+
+	}
+}
---
+++ SummerProject/Assets/Loops/Loops.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loops : MonoBehaviour
+{
+
+	//Add the names in unity
+	public string[] myFamily;
+	string familyArray;
+
+	void Start ()
+	{
+		foreach (string _familyName in myFamily) {
+			familyArray += (_familyName + " loves ");
+			print (familyArray);
+		}
+
+	}
+
+}
