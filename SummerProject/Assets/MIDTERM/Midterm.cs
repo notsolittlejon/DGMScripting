59e137126bb675fc982ced3cd117664cb097e8fa
Jonathan Hardy <thembwun@gmail.com>
2016-06-20 11:34:17 -0600

    Created Scope and Midterm scripts

---
+++ SummerProject/Assets/MIDTERM/Midterm.cs
@@ -0,0 +1,75 @@
+﻿/*
+Write a Text-Based program (game):
+
+Requirements are:
+------------------
+-Variables
+-Functions
+--One function must solve a math problem (Add/Subtract)
+-If Statements 
+-Loops
+--One loop must add bonus inventory items
+-Scope and access modifiers
+-Create Power-Ups
+-
+
+Problem solve the following:
+
+Create power-ups that must
+---------------------------
+-User interaction. (Trigger, Click)
+-Add to a variable (health, ammo)
+
+Create power-up that
+-----------------------
+-Adds an inventory item 
+
+Create a function that can solve a math problem
+-------------------------------------------------
+-Add numbers
+-Subtract numbers
+
+Create a function that runs with a certain answer (If answer is 10 for example, something happens);
+----------------------------------------------------------------------------------------------------
+-Loop and add bonus inventory items
+
+*/
+using UnityEngine;
+using System.Collections;
+
+
+
+public class Midterm : MonoBehaviour
+{
+	
+	public string userName = "Type your name here";
+	public float strength = Random.Range(1,10);
+	public int dexterity = Random.Range(1,10);
+	public int intelligence = Random.Range (1,10);
+	public int charm = Random.Range(1,10);
+
+	void StoryIntro ()
+	{
+		string part1 = "Welcome to this revolutionary text-based adventure. If you haven't done so yet, please input your name in the UserName field, so we can begin.";
+		print (part1);
+
+	}
+
+
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		StoryIntro ();
+		//CharacterCreation (strength, dexterity, intelligence, charm);
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
---
+++ SummerProject/Assets/Scope & Access Modifiers/ScopeAccess.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScopeAccess {
+
+	private int apples = 10;
+	private int oranges = 5;
+	public int bananas;
+
+	public void AddApples(int _moreApples){
+		apples += _moreApples;
+	}
+
+	public void AddOranges (int _moreOranges){
+		oranges += _moreOranges;
+
+	}
+
+	public void AddFruit(int fruit1, int fruit2, int fruit3)
+	{int grapes = 1;
+		int salad = grapes + fruit1 + fruit2 + fruit3;
+		Debug.Log (salad);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		apples = 20;
+		Debug.Log (apples);
+		//V--------------This one won't work, because "grapes" are only visible to the AddFruit function. Wahoo. 
+		//print (grapes);
+		AddFruit(apples, oranges, bananas);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
