e15e9b816163b9d5857acc838135d8e1b149d1ee
Jonathan Hardy <thembwun@gmail.com>
2016-07-11 10:46:00 -0600

    Started Monsters script

---
+++ SummerProject/Assets/MIDTERM/Monster.cs
@@ -0,0 +1,24 @@
+﻿[System.Serializable]
+public class Monster
+{
+	public string monsterName;
+	public int health;
+	public int strength;
+	public int dexterity;
+	public int speed;
+	public int charisma;
+	public int intelligence;
+
+	public void InitializeMonster (int _s, int _d, int _c, int _i)
+	{
+		
+		strength = _s;
+		dexterity = _d;
+		speed = ((_d - _s) * (_d - _s));
+		charisma = _c;
+		intelligence = _i;
+		if (speed == 0) {
+			speed++;
+		}
+	}
+}
