df9790d7bb6bb32d58ce88db4ff76ff4206573fe
Jonathan Hardy <thembwun@gmail.com>
2016-07-29 14:15:27 -0600

    Created Final script and updated Monster script

---
+++ SummerProject/Assets/MIDTERM/Final.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Final : MonoBehaviour {
+
+	Monster Troll = new Monster();
+	Monster Guard = new Monster();
+
+
+	Monster[] monsterArray = new Monster[4];
+	List<Monster> monsterList = new List<Monster>();
+
+	void ListEm(){
+		foreach (Monster aMonster in monsterList) {
+			print (aMonster.monsterName);
+		}
+	}
+
+	void ListEmArray(){
+		foreach (Monster aMonster in monsterArray ){
+			print (aMonster.monsterName);
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		Troll.Spawn ("Troll");
+		//Troll.Initialize ();
+
+		Guard.Spawn ("Guard");
+		//Guard.Initialize ();
+		monsterList.Add (Troll);
+		monsterList.Add (Guard);
+		monsterArray [0] = Troll;
+		monsterArray [1] = Guard;
+		ListEm ();
+		monsterList.Remove (Troll);
+		ListEm ();
+		monsterList.Remove (Guard);
+		ListEm ();
+		ListEmArray ();
+
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ SummerProject/Assets/MIDTERM/Monster.cs
@@ -0,0 +1,28 @@
+﻿[System.Serializable]
+public class Monster
+{
+	public string monsterName;
+	public int health;
+	public int strength;
+	public int dexterity;
+	public int speed;
+	public int charisma;
+	public int intelligence;
+
+	public void Spawn (string _n){
+		monsterName = _n;
+	}
+
+	public void Initialize (int _s, int _d, int _c, int _i)
+	{
+		
+		strength = _s;
+		dexterity = _d;
+		speed = ((_d - _s) * (_d - _s));
+		charisma = _c;
+		intelligence = _i;
+		if (speed == 0) {
+			speed++;
+		}
+	}
+}
