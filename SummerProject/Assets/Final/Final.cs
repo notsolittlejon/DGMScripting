900c5e58abafe59ef494f168ed833f8ea067dfed
Jonathan Hardy <thembwun@gmail.com>
2016-08-03 13:46:51 -0600

    Started on FInal script
    Added buttons and text to UI

---
+++ SummerProject/Assets/Final/Final.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Final : MonoBehaviour {
+
+	public GameObject level;
+	public GameObject monster1;
+	public Button attackB;
+	public Button charmB;
+	public Button runB;
+	public Button contB;
+	public Text story;
+	public Text stats;
+	public enum gameState {Attacking, Charming, Running};
+
+
+
+
+	void Start () {
+
+		monster1.gameObject.SetActive (false);
+		attackB.gameObject.SetActive (false);
+		charmB.gameObject.SetActive (false);
+		runB.gameObject.SetActive (false);
+		contB.gameObject.SetActive (true);
+
+
+
+
+
+	}
+	public void StartThisGame(){
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
